1c4525ab61eb914be36b928532cbcec24f007af5
Hubert Holierhoek <Hubert.Holierhoek@nokia.com>
2018-11-21 12:57:41 -0500

    Various enhancements

--- src/main/java/org/gnieh/logback/config/ConfigConfigurator.java
+++ src/main/java/org/gnieh/logback/config/ConfigConfigurator.java
@@ -16,34 +16,43 @@
  */
 package org.gnieh.logback.config;
 
-import java.lang.management.ManagementFactory;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import ch.qos.logback.classic.jmx.JMXConfigurator;
-import ch.qos.logback.classic.jmx.MBeanUtil;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import com.typesafe.config.ConfigObject;
-import com.typesafe.config.ConfigValue;
-
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.jmx.JMXConfigurator;
+import ch.qos.logback.classic.jmx.MBeanUtil;
 import ch.qos.logback.classic.spi.Configurator;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.Appender;
+import ch.qos.logback.core.CoreConstants;
 import ch.qos.logback.core.FileAppender;
+import ch.qos.logback.core.joran.spi.ConfigurationWatchList;
+import ch.qos.logback.core.joran.util.ConfigurationWatchListUtil;
 import ch.qos.logback.core.joran.util.beans.BeanDescriptionCache;
 import ch.qos.logback.core.rolling.RollingPolicy;
 import ch.qos.logback.core.spi.ContextAwareBase;
 import ch.qos.logback.core.spi.LifeCycle;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigObject;
+import com.typesafe.config.ConfigValue;
+import com.typesafe.config.ConfigValueType;
 
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
+import java.io.File;
+import java.lang.management.ManagementFactory;
+import java.net.URL;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.ServiceLoader;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A configurator using Typesafe's config library to lookup and load logger
@@ -60,7 +69,15 @@ public class ConfigConfigurator extends ContextAwareBase implements Configurator
 
         BeanDescriptionCache beanCache = new BeanDescriptionCache(loggerContext);
 
-        final Config config = ConfigFactory.load();
+        ConfigFactory factory = getFactory();
+        Config config;
+        try {
+            config = factory.load();
+        } catch (Throwable t) {
+            addError("Unable to load Typesafe config", t);
+            return;
+        }
+
         // get the logback configuration root
         final String logbackConfigRoot = config.getString("logback-root");
         // load the configuration per config loading rules
@@ -81,21 +98,15 @@ public class ConfigConfigurator extends ContextAwareBase implements Configurator
             }
         }
 
-        if (logbackConfig.hasPath("root")) {
-            if (logbackConfig.getValue("root") instanceof ConfigObject) {
-                configureLogger(loggerContext, appenders, Logger.ROOT_LOGGER_NAME, logbackConfig.getConfig("root"), true);
-            } else {
-                addWarn("Invalid ROOT logger configuration. Ignoring it.");
-            }
+        ConfigValue rootValue = getValueIfType(logbackConfig.root(), "root", ConfigValueType.OBJECT);
+        if (rootValue != null) {
+            configureLogger(loggerContext, appenders, Logger.ROOT_LOGGER_NAME, (ConfigObject) rootValue, true);
         }
 
-        Config loggerConfigs = logbackConfig.getConfig("loggers");
-        for (Entry<String, ConfigValue> entry : loggerConfigs.root().entrySet()) {
-            if (entry.getValue() instanceof ConfigObject) {
-                configureLogger(loggerContext, appenders, entry.getKey(),
-                        loggerConfigs.getConfig("\"" + entry.getKey() + "\""), false);
-            } else {
-                addWarn(String.format("Invalid logger configuration %s. Ignoring it.", entry.getKey()));
+        ConfigObject loggerConfigs = logbackConfig.getObject("loggers");
+        for (Entry<String, ConfigValue> entry : loggerConfigs.entrySet()) {
+            if (entry.getValue().valueType() == ConfigValueType.OBJECT) {
+                configureLogger(loggerContext, appenders, entry.getKey(), (ConfigObject) entry.getValue(), false);
             }
         }
 
@@ -127,11 +138,79 @@ public class ConfigConfigurator extends ContextAwareBase implements Configurator
                 // registered. Unregistering an MBean within invocation of itself
                 // caused jconsole to throw an NPE. (This occurs when the reload* method
                 // unregisters the
-                JMXConfigurator jmxConfigurator = new JMXConfigurator((LoggerContext) context, mbs, objectName);
+                JMXConfigurator jmxConfigurator = new JMXConfigurator(loggerContext, mbs, objectName);
                 try {
                     mbs.registerMBean(jmxConfigurator, objectName);
                 } catch (Exception e) {
                     addError("Failed to create mbean", e);
+                }
+            }
+        }
+
+        // Use a LinkedHashSet so order is preserved. We use the first one in as the 'main' URL, under the assumption
+        // that maybe that matters somehow to logback. The way we traverse the config tree means that the first one we
+        // add will be one that is closer to the root of the tree.
+        if (registerFileWatchers(loggerContext, getSourceFiles(config.root(), new LinkedHashSet<>()))) {
+            createChangeTask(loggerContext, logbackConfig);
+        }
+    }
+
+    /**
+     * Get the correct config factory object.
+     *
+     * @return the config factory
+     */
+    private ConfigFactory getFactory() {
+        Iterator<ConfigFactory> loaders = ServiceLoader.load(ConfigFactory.class).iterator();
+        if (loaders.hasNext()) {
+            return loaders.next();
+        }
+        return this::defaultFactory;
+    }
+
+    /**
+     * Default config factory method.
+     *
+     * @return the basic TS-config loading
+     */
+    private Config defaultFactory() {
+        return com.typesafe.config.ConfigFactory.load();
+    }
+
+    /**
+     * Create and schedule the task to check for changes to the watch list.
+     *
+     * @param loggerContext the logger context
+     * @param config        the logback TS-config
+     */
+    private void createChangeTask(LoggerContext loggerContext, Config config) {
+        if (config.hasPath("scanPeriod")) {
+            Duration scanPeriod = config.getDuration("scanPeriod");
+            if (scanPeriod != null) {
+                long delay = scanPeriod.toMillis();
+                if (delay > 0) {
+                    Runnable rocTask = () -> {
+                        ConfigurationWatchList configurationWatchList =
+                                ConfigurationWatchListUtil.getConfigurationWatchList(context);
+                        if (configurationWatchList == null) {
+                            addWarn("Null ConfigurationWatchList in context");
+                            return;
+                        }
+                        List<File> filesToWatch = configurationWatchList.getCopyOfFileWatchList();
+                        if (filesToWatch == null || filesToWatch.isEmpty()) {
+                            addInfo("Empty watch file list. Disabling ");
+                            return;
+                        }
+                        if (!configurationWatchList.changeDetected()) {
+                            return;
+                        }
+                        loggerContext.reset();
+                        configure(loggerContext);
+                    };
+
+                    loggerContext.putObject(CoreConstants.RECONFIGURE_ON_CHANGE_TASK, rocTask);
+                    loggerContext.addScheduledFuture(loggerContext.getScheduledExecutorService().
+                            scheduleAtFixedRate(rocTask, delay, delay, TimeUnit.MILLISECONDS));
                 }
             }
         }
@@ -219,37 +298,116 @@ public class ConfigConfigurator extends ContextAwareBase implements Configurator
     }
 
     private void configureLogger(LoggerContext loggerContext, Map<String, Appender<ILoggingEvent>> appenders,
-                                 String name, Config config, boolean isRoot) {
+                                 String name, ConfigObject config, boolean isRoot) {
         final Logger logger = loggerContext.getLogger(name);
 
-        if (config.hasPathOrNull("level")) {
-            if (config.getIsNull("level") && isRoot) {
-                addWarn("Log level NULL is not authorized for ROOT logger");
-            } else if (!config.getIsNull("level")) {
-                String levelName = config.getString("level");
-                if (isRoot && (levelName.equalsIgnoreCase("NULL") || levelName.equalsIgnoreCase("INHERITED"))) {
-                    addWarn(String.format("Log level %s is not authorized for ROOT logger.", levelName.toUpperCase()));
-                } else if (!levelName.equalsIgnoreCase("NULL") && !levelName.equalsIgnoreCase("INHERITED")) {
-                    logger.setLevel(Level.toLevel(config.getString("level")));
+        ConfigValue levelValue = getValueIfType(config, "level", ConfigValueType.STRING);
+        if (levelValue != null) {
+            String levelName = (String) levelValue.unwrapped();
+            boolean isRestricted = levelName.equalsIgnoreCase("NULL") ||
+                    levelName.equalsIgnoreCase("INHERITED");
+            if (isRoot && isRestricted) {
+                addWarn(String.format("Log level %s is not authorized for ROOT logger.", levelName.toUpperCase()));
+            } else if (!isRestricted) {
+                logger.setLevel(Level.toLevel(levelName));
+            }
+        }
+
+        ConfigValue additivityValue = getValueIfType(config, "additivity", ConfigValueType.BOOLEAN);
+        if (additivityValue != null) {
+            logger.setAdditive((Boolean) additivityValue.unwrapped());
+        }
+
+        ConfigValue appendersValue = getValueIfType(config, "appenders", ConfigValueType.LIST);
+        if (appendersValue != null) {
+            List appenderRefs = (List) appendersValue.unwrapped();
+            for (Object appenderRef : appenderRefs) {
+                if (appenderRef instanceof String) {
+                    if (appenders.containsKey(appenderRef)) {
+                        logger.addAppender(appenders.get(appenderRef));
+                    } else {
+                        addWarn(String.format("Unknown appender %s. Ignoring it.", appenderRef));
+                    }
                 }
             }
         }
 
-        if (config.hasPath("additivity")) {
-            logger.setAdditive(config.getBoolean("additivity"));
-        }
-
-        if (config.hasPath("appenders")) {
-            List<String> appenderRefs = config.getStringList("appenders");
-            for (String appenderRef : appenderRefs) {
-                if (appenders.containsKey(appenderRef)) {
-                    logger.addAppender(appenders.get(appenderRef));
-                } else {
-                    addWarn(String.format("Unknown appender %s. Ignoring it.", appenderRef));
+        if (!isRoot) {
+            for (Entry<String, ConfigValue> entry : config.entrySet()) {
+                if (entry.getValue().valueType() == ConfigValueType.OBJECT) {
+                    configureLogger(loggerContext, appenders, name + '.' + entry.getKey(),
+                            (ConfigObject) entry.getValue(), false);
                 }
             }
         }
-
     }
 
+    /**
+     * Get the value of the object at the given path, only if it's of the given type.
+     *
+     * @param config the config from which the get the object
+     * @param path   the config path at which the object is to be found
+     * @param type   the object type to return
+     *
+     * @return the object, or null if it doesn't exist or isn't of the requested type
+     */
+    private ConfigValue getValueIfType(ConfigObject config, String path, ConfigValueType type) {
+        if (config.containsKey(path) && config.get(path).valueType() == type) {
+            return config.get(path);
+        }
+        return null;
+    }
+
+    /**
+     * Find all real source files in the config. This does not include those encapsulated in jars, etc. Only those that
+     * are directly in the file system.
+     *
+     * @param config the TS-config
+     * @param files  the set to add newly found files to
+     *
+     * @return the set of files found, as URLs
+     */
+    private Set<URL> getSourceFiles(ConfigValue config, Set<URL> files) {
+        getSourceFile(config, files);
+        if (config.valueType() == ConfigValueType.OBJECT) {
+            for (ConfigValue value : ((ConfigObject)config).values()) {
+                getSourceFiles(value, files);
+            }
+        }
+        return files;
+    }
+
+    /**
+     * Get a single source file from the given config value, if it is a real file.
+     *
+     * @param value the TS-config value
+     * @param files the set of files to add the newly found file to
+     */
+    private void getSourceFile(ConfigValue value, Set<URL> files) {
+        if (value.origin().filename() != null) {
+            // Check 'contains' first to avoid re-adding, and messing with the order
+            if (!files.contains(value.origin().url())) {
+                files.add(value.origin().url());
+            }
+        }
+    }
+
+    /**
+     * Register all real config files to be watched by logback.
+     *
+     * @param loggerContext the logger context
+     * @param sourceFiles   the source files to watch
+     *
+     * @return true if there were any files to watch
+     */
+    private boolean registerFileWatchers(LoggerContext loggerContext, Set<URL> sourceFiles) {
+        Iterator<URL> iterator = sourceFiles.iterator();
+        if (iterator.hasNext()) {
+            ConfigurationWatchListUtil.setMainWatchURL(loggerContext, iterator.next());
+            iterator.forEachRemaining(u -> ConfigurationWatchListUtil.addToWatchList(loggerContext, u));
+            return true;
+        }
+        addWarn("No configuration files to watch, so no file scanning is possible");
+        return false;
+    }
 }
